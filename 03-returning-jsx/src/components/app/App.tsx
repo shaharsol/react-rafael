5a9aa5f3010ed3a0fd96cf27ff5f9a08c708ee8e
shaharsol <shahar@gnift.com>
2025-12-01 14:30:45 +0200

    after embeddings

---
+++ 03-returning-jsx/src/components/app/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+    // js
+    // ts
+    //...
+
+    return (
+        <>
+            <div className="App">
+                <p>hi from App.tsx</p>
+            </div>
+            <p>i dont need css</p>
+        </>
+    )
+
+}
+
+export default App
---
+++ 03-returning-jsx/src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './components/app/App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+      <p>hi from main.tsx</p>
+    <App />
+  </StrictMode>,
+)
