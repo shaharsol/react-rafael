5f72cbef4eafb5dcb161dbc3a499f4a1e06914a8
shaharsol <shahar@gnift.com>
2025-12-08 15:49:42 +0200

    profile complete with li

--- 10-server-access/src/components/posts/profile/Profile.tsx
+++ 10-server-access/src/components/posts/profile/Profile.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react'
 import './Profile.css'
+import type Post from '../../../models/Post'
+import profileService from '../../../services/profile'
 
 export default function Profile() {
+
+    const [ posts, setPosts] = useState<Post[]>([])
+
+    useEffect(() => {
+
+        // (async () => {
+        //     const posts = await profileService.getProfile()
+
+        // })()
+        profileService.getProfile()
+            .then(setPosts)
+            .catch(e => alert(e.message))
+    }, [])
+
     return (
         <div className='Profile'>
-            my profile...
+            <ul>
+                {posts.map(({id, title}) => <li key={id}>{title}</li>)}
+            </ul>
         </div>
     )
 }
